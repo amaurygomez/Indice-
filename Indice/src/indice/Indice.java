fa3077df0724a9f1006617677e5d3751008fabc5
Amaury Gomez <amaurygomez1993@gmail.com>
2012-09-04 14:15:42 -0400

    Programa

---
+++ Indice/src/indice/Indice.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indice;
+
+/**
+ *
+ * @author Amaury
+ */
+public class Indice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
